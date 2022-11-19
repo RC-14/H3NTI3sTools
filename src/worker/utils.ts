260fca2ee1ebad3e6100c25a1a0eb6dae54c8f16
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-11-19 16:40:03 +0100

    Create sendMessageToFrame function

--- src/worker/utils.ts
+++ src/worker/utils.ts
@@ -1,6 +1,6 @@
 export { default as StorageHelper } from '../StorageHelper.js';
 export { default as htmlCharRef } from '../htmlCharReferences.js';
-export { generateIDBGetter, isValidUrl } from '../utils.js'
+export { generateIDBGetter, isValidUrl } from '../utils.js';
 
 export const sendMessageToTab: sendMessageToTab = (tabId, handler, msg, data) => {
 	return chrome.tabs.sendMessage<RuntimeMessage>(tabId, {
@@ -9,4 +9,13 @@ export const sendMessageToTab: sendMessageToTab = (tabId, handler, msg, data) =>
 		msg,
 		data
 	});
+};
+
+export const sendMessageToFrame: sendMessageToFrame = (tabId, frameId, handler, msg, data) => {
+	return chrome.tabs.sendMessage<RuntimeMessage>(tabId, {
+		target: 'content',
+		handler,
+		msg,
+		data
+	}, { frameId });
 };
--- types/messaging.d.ts
+++ types/messaging.d.ts
@@ -7,5 +7,6 @@ interface RuntimeMessage {
 
 type sendRuntimeMessage = (handler: string, msg: string | null, data?: any) => Promise<any>;
 type sendMessageToTab = (tabId: number, handler: string, msg: string | null, data?: any) => Promise<any>;
+type sendMessageToFrame = (tabId: number, frameId: number, handler: string, msg: string | null, data?: any) => Promise<any>;
 
 type RuntimeMessageHandler = (msg: string | null, data: any, sender: chrome.runtime.MessageSender) => any;
