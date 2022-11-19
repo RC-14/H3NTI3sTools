584488cfa7fcc372402a5a52b26ecc36ca5e6753
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-11-19 12:17:21 +0100

    Make all custom types available in the worker

--- src/worker/linkToGlobals.d.ts
+++ src/worker/linkToGlobals.d.ts
@@ -1,1 +1,4 @@
+/// <reference path="../../types/general.d.ts" />
 /// <reference path="../../types/messaging.d.ts" />
+/// <reference path="../../types/pixiv.d.ts" />
+/// <reference path="../../types/pixivViewer.d.ts" />
