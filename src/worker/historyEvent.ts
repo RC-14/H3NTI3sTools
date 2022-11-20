58a18155064152b75a52a9777f23b0d24ab06cfa
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-11-20 21:44:38 +0100

    Wrap sending the historyEvent massage in try catch

--- src/worker/historyEvent.ts
+++ src/worker/historyEvent.ts
@@ -4,7 +4,11 @@ const module: ModuleObject = { id: 'historyEvent' };
 
 module.historyStateUpdatedHandler = (details) => {
 	if (details.url.startsWith('chrome')) return;
-	sendMessageToFrame(details.tabId, details.frameId, module.id, null);
+
+	// Wrapped in try catch because this can fail if the content script isn't loaded for whatever reason
+	try {
+		sendMessageToFrame(details.tabId, details.frameId, module.id, null);
+	} catch (error) { }
 };
 
 export default module;
