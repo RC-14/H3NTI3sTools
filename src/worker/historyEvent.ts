0960c9101a7ed2709b89b9ff0a6ce456905db845
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-11-20 21:53:53 +0100

    Try catch didn't work but catch() does

--- src/worker/historyEvent.ts
+++ src/worker/historyEvent.ts
@@ -5,10 +5,10 @@ const module: ModuleObject = { id: 'historyEvent' };
 module.historyStateUpdatedHandler = (details) => {
 	if (details.url.startsWith('chrome')) return;
 
-	// Wrapped in try catch because this can fail if the content script isn't loaded for whatever reason
-	try {
-		sendMessageToFrame(details.tabId, details.frameId, module.id, null);
-	} catch (error) { }
+	sendMessageToFrame(details.tabId, details.frameId, module.id, null).catch((reason) => {
+		// Reload tab if the content script isn't injected
+		chrome.tabs.reload(details.tabId);
+	});
 };
 
 export default module;
