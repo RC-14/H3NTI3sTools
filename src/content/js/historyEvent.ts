f6c55a5aeffeaa89724868f214e17ea4441b88d3
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-11-20 21:35:47 +0100

    Get rid of old check in content historyEvent.ts

--- src/content/js/historyEvent.ts
+++ src/content/js/historyEvent.ts
@@ -1,6 +1,4 @@
 chrome.runtime.onMessage.addListener((message: RuntimeMessage, sender, sendResponse) => {
-	if (message.target !== 'content' || message.handler !== 'historyEvent') return;
-
 	dispatchEvent(new Event('historystateupdated'));
 	sendResponse();
 });
