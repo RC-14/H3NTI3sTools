5e4108dbe4c86e9f15f95a34350e037964714a32
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-11-30 23:50:50 +0100

    Stop showing showError for worker historyEvent

--- src/worker/historyEvent.ts
+++ src/worker/historyEvent.ts
@@ -5,13 +5,10 @@ const module: ModuleObject = { id: 'historyEvent' };
 module.historyStateUpdatedHandler = (details) => {
 	if (details.url.startsWith('chrome')) return;
 
-
 	sendMessageToFrame(details.tabId, details.frameId, module.id, null).catch((reason) => {
-
 		// Try to reload the tab in case the content script isn't injected (e.g. after extension reload)
 		chrome.tabs.reload(details.tabId).catch((reason) => {
 			// May fail if the tab doesn't exist for some reason (this catch is here because that happened)
-			chrome.tabs.create({ active: true, url: 'sites/showError/index.html?' + btoa(`${details.url}\n${reason}`) });
 		});
 	});
 };
