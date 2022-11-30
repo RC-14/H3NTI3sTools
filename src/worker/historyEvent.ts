96769c6a2df1b78c672cbf9e3dac4eb82ee5def0
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-11-30 22:12:25 +0100

    Use the showError site in worker historyEvent.ts

--- src/worker/historyEvent.ts
+++ src/worker/historyEvent.ts
@@ -5,9 +5,14 @@ const module: ModuleObject = { id: 'historyEvent' };
 module.historyStateUpdatedHandler = (details) => {
 	if (details.url.startsWith('chrome')) return;
 
+
 	sendMessageToFrame(details.tabId, details.frameId, module.id, null).catch((reason) => {
-		// Reload tab if the content script isn't injected
-		chrome.tabs.reload(details.tabId);
+
+		// Try to reload the tab in case the content script isn't injected (e.g. after extension reload)
+		chrome.tabs.reload(details.tabId).catch((reason) => {
+			// May fail if the tab doesn't exist for some reason (this catch is here because that happened)
+			chrome.tabs.create({ active: true, url: 'sites/showError/index.html?' + btoa(`${details.url}\n${reason}`) });
+		});
 	});
 };
 
