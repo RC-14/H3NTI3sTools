a772b59918a919f716fdb4f861cdeb3e43a67489
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-01-06 04:26:40 +0100

    Add loadHighResButton to kemono

---
+++ src/content/js/kemono.ts
@@ -0,0 +1,46 @@
+import { qs, qsa, showMessage } from '../../utils.js';
+
+const loadHighResButton = document.createElement('button');
+loadHighResButton.id = 'h3nti3-load-high-res-button';
+loadHighResButton.innerText = 'Load high res images';
+
+loadHighResButton.addEventListener('click', (event) => {
+	if (document.readyState !== 'complete') {
+		showMessage("Page didn't finish loading yet...", {
+			color: 'yellow',
+			size: 'xx-large'
+		});
+		return;
+	}
+
+	const imgElements = qsa<HTMLImageElement>('.post__thumbnail > .fileThumb.image-link > img:only-child');
+
+	// The images need to be clicked to trigger an event listener that then loads the high res version.
+	imgElements.forEach(elem => elem.click());
+
+	// The button is now useless so we might as well remove it.
+	loadHighResButton.remove();
+});
+
+const appendButton = () => {
+	const postFilesContainer = qs<HTMLDivElement>('div.post__files');
+
+	if (!(postFilesContainer instanceof HTMLDivElement)) throw new Error("Can't find post files container.");
+
+	postFilesContainer.before(loadHighResButton);
+};
+
+// Make sure the page shows a post
+if (location.pathname.match(/^\/\w+\/user\/\d+\/post\/\d+/i)) {
+	// Depending on the internet connection appending the button might fail.
+	try {
+		appendButton();
+	} catch (error) {
+		// In that case we wait for the page to load and retry.
+		document.addEventListener('readystatechange', () => {
+			if (document.readyState === 'loading') return;
+
+			appendButton();
+		}, { once: true });
+	}
+}
