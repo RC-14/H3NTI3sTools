f780b639c03ab78831429e9082e367bfad248119
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-01-06 05:16:24 +0100

    Remove duplicate images on kemono

--- src/content/js/kemono.ts
+++ src/content/js/kemono.ts
@@ -1,14 +1,39 @@
-import { qsa } from '../../utils.js';
+import { qs, qsa } from '../../utils.js';
 
+// For some reason the first image is shown twice but it won't hurt to remove other duplicates as well.
+const removeDuplicateImages = () => {
+	const imgWrappers = Array.from(qsa<HTMLDivElement>('.post__files > div.post__thumbnail'));
+	const imgSrcs = imgWrappers.map(wrapper => qs<HTMLImageElement>('img[data-src]', wrapper)?.dataset.src)
+
+	imgSrcs.forEach((src, i) => {
+		if (src === undefined) return;
+		if (imgSrcs.indexOf(src) === i) return;
+
+		imgWrappers[i].remove();
+	});
+};
+
+// Images are in a lower resolution until you click on them.
 const loadHighResImages = () => {
-	const imgElements = qsa<HTMLImageElement>('.post__thumbnail > .fileThumb.image-link > img:only-child');
+	const imgElements = qsa<HTMLImageElement>('.post__files > .post__thumbnail > .fileThumb.image-link > img:only-child');
 
-	// The images need to be clicked to trigger an event listener that then loads the high res version.
 	imgElements.forEach(elem => elem.click());
 };
 
 // Make sure the page shows a post
 if (location.pathname.match(/^\/\w+\/user\/\d+\/post\/\d+/i)) {
+	// Remove duplicates
+	if (document.readyState !== 'loading') {
+		removeDuplicateImages();
+	} else {
+		document.addEventListener('readystatechange', () => {
+			if (document.readyState !== 'interactive') return;
+
+			removeDuplicateImages();
+		});
+	}
+
+	// Load High res versions
 	if (document.readyState === 'complete') {
 		loadHighResImages();
 	} else {
