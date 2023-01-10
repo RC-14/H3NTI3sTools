070c8e063acd10580fcce6cab47f1d9ff81cce67
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-01-10 03:23:15 +0100

    Improve kemono.ts

--- src/content/js/kemono.ts
+++ src/content/js/kemono.ts
@@ -1,9 +1,16 @@
 import { qs, qsa } from '../../utils.js';
 
-// For some reason the first image is shown twice but it won't hurt to remove other duplicates as well.
+// For some reason the first image is shown twice (sometimes the first image is a cropped version of the second image)
+// but it won't hurt to remove other duplicates as well.
 const removeDuplicateImages = () => {
 	const imgWrappers = Array.from(qsa<HTMLDivElement>('.post__files > div.post__thumbnail'));
-	const imgSrcs = imgWrappers.map(wrapper => qs<HTMLAnchorElement>('a.image-link', wrapper)?.href)
+
+	if (imgWrappers.length < 2) return;
+
+	// Remove the first image
+	imgWrappers.splice(0, 1)[0].remove();
+
+	const imgSrcs = imgWrappers.map(wrapper => qs<HTMLAnchorElement>('a.image-link', wrapper)?.href);
 
 	imgSrcs.forEach((src, i) => {
 		if (src === undefined) return;
@@ -15,9 +22,39 @@ const removeDuplicateImages = () => {
 
 // Images are in a lower resolution until you click on them.
 const loadHighResImages = () => {
-	const imgElements = qsa<HTMLImageElement>('.post__files > .post__thumbnail > .fileThumb.image-link > img:only-child');
+	const imgElements = qsa<HTMLImageElement>('.post__files > .post__thumbnail > a.fileThumb.image-link > img[data-src]');
 
-	imgElements.forEach(elem => elem.click());
+	imgElements.forEach((imgElement, i) => {
+		// Prevent triggering other event listeners and redirects
+		imgElement.addEventListener('click', (event) => {
+			event.stopImmediatePropagation();
+			event.preventDefault();
+		});
+
+		// Images sometimes fail to load the first time so we try multiple times.
+		let failCounter = 0;
+		imgElement.addEventListener('error', (event) => {
+			if (failCounter === 5) return;
+			failCounter++;
+
+			setTimeout(() => {
+				const imgLink = imgElement.parentElement as HTMLAnchorElement;
+				imgElement.src = '';
+				imgElement.src = imgLink.href;
+			}, 100);
+		});
+
+		imgElement.addEventListener('load', (event) => {
+			console.log(`Loaded img ${i + 1}/${imgElements.length}`);
+		});
+
+		// Remove data-src attribute to prevent mistaking it for a low res image
+		delete imgElement.dataset.src;
+
+		// The src for the high res version is in the href attribute of the parent a element
+		const imgLink = imgElement.parentElement as HTMLAnchorElement;
+		imgElement.src = imgLink.href;
+	});
 };
 
 // Make sure the page shows a post
