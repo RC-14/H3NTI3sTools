ec1603a2a7dcf3f07aafd8f4b513a1d1c6716c40
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-04-29 04:50:09 +0200

    Use single quotes

--- web-ext-config.js
+++ web-ext-config.js
@@ -1,15 +1,15 @@
 module.exports = {
-	sourceDir: "dist",
-	artifactsDir: "build",
+	sourceDir: 'dist',
+	artifactsDir: 'build',
 	build: {
 		// {path.to.value} will be replaced with whatever value is there in the manifest
-		filename: "{short_name}_v{version}.zip",
+		filename: '{short_name}_v{version}.zip',
 		overwriteDest: true
 	},
 	run: {
-		target: ["firefox-desktop"],
-		firefox: "firefoxdeveloperedition",
-		firefoxProfile: "dev-edition-default",
+		target: ['firefox-desktop'],
+		firefox: 'firefoxdeveloperedition',
+		firefoxProfile: 'dev-edition-default',
 		keepProfileChanges: false
 	}
-}
+};
