501bbafe88e60e2d88a6c24d98767777e7370384
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-04-30 04:12:17 +0200

    Foundation for BackgroundFragments

---
+++ src/background/loader.ts
@@ -0,0 +1,16 @@
+import { BackgroundFragment } from '/src/types/fragments';
+
+// @ts-ignore
+import * as __unsafe_fragments__ from './*/fragment.ts';
+
+const fragments: Map<string, BackgroundFragment> = new Map(
+	Object.entries(__unsafe_fragments__ as { [key: string]: BackgroundFragment; })
+);
+
+/*
+ * Register Handlers
+ */
+
+for (const id of fragments.keys()) {
+	const fragment = fragments.get(id);
+}
