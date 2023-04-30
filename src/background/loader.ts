fd5e8b121de640f2172e8d597bf0cd3e738a4124
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-04-30 19:34:50 +0200

    Comment why @ts-ignore is necessary at glob import

--- src/background/loader.ts
+++ src/background/loader.ts
@@ -1,6 +1,6 @@
 import { BackgroundFragment } from '/src/lib/fragments';
 
-// @ts-ignore
+// @ts-ignore Necessary because typescript doesn't know that parcel does glob imports
 import * as __unsafe_fragments__ from './*/fragment.ts';
 
 const fragments: Map<string, BackgroundFragment> = new Map(
