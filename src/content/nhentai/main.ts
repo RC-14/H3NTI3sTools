2e607e1dc2bf550a81316f912ae25f0cd4eb81fc
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-05-10 22:35:25 +0200

    Migrate nhentai content scripts

---
+++ src/content/nhentai/main.ts
@@ -0,0 +1,45 @@
+import { qs, isElementEditable } from '/src/lib/utils';
+import { addShortcuts } from '/src/lib/nextPreviousShortcuts';
+import state from './state';
+import { applyCustomReaderSettings } from './readerSettingsUtils';
+
+const next = () => {
+	qs<HTMLAnchorElement>('a.next')?.click();
+};
+const previous = () => {
+	qs<HTMLAnchorElement>('a.previous')?.click();
+};
+
+if (state === 'browsing' || state === 'searching') {
+	addShortcuts(next, previous);
+} else if (state === 'reading') {
+	applyCustomReaderSettings();
+
+	document.addEventListener('keydown', (event) => {
+		if (isElementEditable(event.target as HTMLElement)) return;
+
+		switch (event.code) {
+			case 'Escape':
+				setTimeout(() => {
+					const back = qs('a.go-back');
+					if (back instanceof HTMLAnchorElement) back.click();
+				}, 100);
+				break;
+
+			case 'Space':
+				if (event.shiftKey) {
+					previous();
+				} else {
+					next();
+				}
+				break;
+		}
+	});
+} else if (state === 'lookingAtGallery') {
+	document.addEventListener('keydown', (event) => {
+		if (event.code !== 'Space' || isElementEditable(event.target as HTMLElement)) return;
+
+		const firstPageLink = qs('#cover a');
+		if (firstPageLink instanceof HTMLAnchorElement) firstPageLink.click();
+	});
+};
