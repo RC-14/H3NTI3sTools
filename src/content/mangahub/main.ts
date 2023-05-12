e52c108a870b88e8eb02f53ca95de46dea0314cc
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-05-12 15:47:03 +0200

    Slight improvements to the mangahub content script

--- src/content/mangahub/main.ts
+++ src/content/mangahub/main.ts
@@ -1,22 +1,9 @@
 import { addNextPreviousShortcuts } from '/src/lib/nextPreviousShortcuts';
-import { qs, qsa } from '/src/lib/utils';
+import { qs } from '/src/lib/utils';
 
-// Stop MangaHub from changing the page with JS instead of using a redirect
-document.addEventListener('click', (event) => {
-	if (!(event.target instanceof HTMLAnchorElement)) return;
-	if (!event.target.href.length) return;
-
-	event.preventDefault();
-	open(event.target.href, '_self', 'noreferrer');
-});
-
-// Sadly MangaHub loads parts of the page only when the user scrolls to it.
-document.addEventListener('scroll', () => {
-	// Remove image numbers
-	qsa('._3w1ww').forEach((elem) => elem.remove());
-	// Remove the ugly white box at the bottom
+const removeUglyWhitBoxAtBottom = () => {
 	qs('._1QUKS')?.remove();
-});
+};
 
 const previousChapter = () => {
 	qs<HTMLAnchorElement>('.previous > a')?.click();
@@ -25,4 +12,19 @@ const nextChapter = () => {
 	qs<HTMLAnchorElement>('.next > a')?.click();
 };
 
+// Stop MangaHub from changing the page with JS instead of using a redirect
+document.addEventListener('click', (event) => {
+	if (!(event.target instanceof HTMLAnchorElement)) return;
+	if (!event.target.href.length) return;
+
+	event.preventDefault();
+	open(event.target.href, '_self');
+});
+
 addNextPreviousShortcuts(nextChapter, previousChapter);
+
+if (document.readyState !== 'loading') {
+	removeUglyWhitBoxAtBottom();
+} else {
+	document.addEventListener('readystatechange', removeUglyWhitBoxAtBottom, { once: true });
+}
