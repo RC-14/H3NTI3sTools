3b9d5d795ddd95a3a4d2c659461f2a177f35f6a7
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-05-12 13:32:52 +0200

    Fix content not getting hidden

--- src/content/kemono/hideContent.ts
+++ src/content/kemono/hideContent.ts
@@ -1,14 +1,11 @@
 import { qs, qsa } from '/src/lib/utils';
 
-const content = qs<HTMLDivElement>('div.post__content')
-let showContentButton: HTMLButtonElement | undefined;
-
-const showContentListener = (event: MouseEvent) => showContentButton?.replaceWith(content!);
-
 // Most of the time I don't want to see the content but the files instead, so I just hide the content
 export const hideContent = () => {
 	// If there are no files I probably want to see the content
 	if (qs('.post__files') === null) return;
+
+	const content = qs<HTMLDivElement>('div.post__content');
 
 	// There may be no content
 	if (!(content instanceof HTMLDivElement)) return;
@@ -18,10 +15,10 @@ export const hideContent = () => {
 		img.decoding = 'async';
 	}
 
-	showContentButton = document.createElement('button');
+	const showContentButton = document.createElement('button');
 	showContentButton.innerText = '...';
 
-	showContentButton.addEventListener('click', showContentListener, { once: true, passive: true });
+	showContentButton.addEventListener('click', (event: MouseEvent) => showContentButton.replaceWith(content), { once: true, passive: true });
 
 	content.replaceWith(showContentButton);
 };
