011858ee5f69bee64cdd7f9e4fe1d8ca1fff56f5
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-05-14 21:09:44 +0200

    Add a (for now) useless popup

---
+++ src/popup/script.ts
@@ -0,0 +1,35 @@
+import { runtime } from 'webextension-polyfill';
+import { qs, useTemplate } from '../lib/utils';
+
+const tabList: { name: string, fragmentId: string; }[] = [
+];
+
+const mainFrame = qs<HTMLIFrameElement>('main > iframe');
+const navbar = qs<HTMLUListElement>('ul#navbar');
+const navbarItemTemplate = qs<HTMLTemplateElement>('template#navbar-item-template');
+
+if (!(mainFrame && navbar && navbarItemTemplate)) throw new Error("HTML elements not found.");
+
+navbar.addEventListener('click', (event) => {
+	if (!(event.target instanceof HTMLLIElement)) return;
+
+	const prevSelected = qs('.navbar-item.selected');
+	if (prevSelected) prevSelected.classList.remove('selected');
+
+	event.target.classList.add('selected');
+
+	const fragmentId = event.target.dataset.path;
+	if (fragmentId == null) return;
+	mainFrame.src = runtime.getURL(`popup/${fragmentId}/index.html`);
+});
+
+for (const tab of tabList) {
+	const navbarItem = useTemplate(navbarItemTemplate);
+	if (!(navbarItem instanceof HTMLLIElement)) throw new Error("Navbar item isn't an LI element.");
+
+	navbarItem.dataset.path = tab.fragmentId;
+	navbarItem.textContent = tab.name;
+	navbar.appendChild(navbarItem);
+}
+
+qs<HTMLLIElement>('.navbar-item', navbar)?.click();
