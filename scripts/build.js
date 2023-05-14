6ace48c448860d82e32250e79871d49e750472a1
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-05-14 21:12:39 +0200

    Add comments to the build script

--- scripts/build.js
+++ scripts/build.js
@@ -5,6 +5,7 @@ import { join as joinPath } from 'path';
 
 const MANIFEST_PATH = 'src/manifest.json';
 
+// Check if the manifest exists
 try {
 	const info = await stat(MANIFEST_PATH);
 	if (!info.isFile()) throw new Error(`Manifest is not a file. (${MANIFEST_PATH})`);
@@ -12,12 +13,15 @@ try {
 	throw new Error(`Manifest not found. (${MANIFEST_PATH})`);
 }
 
+// Parse the manifest as JSON (may throw but fixing a broken manifest is out of scope)
 const manifest = JSON.parse(await readFile(MANIFEST_PATH, { encoding: 'utf-8' }));
 
+// Get paths to all files in a directory
 const getAllFiles = async (dirPath, filter) => {
 	const result = [];
 	const directories = [dirPath];
 
+	// Use a while loop to avoid recursion
 	while (directories.length > 0) {
 		const directory = directories.splice(0, 1)[0];
 		const files = await readdir(directory);
@@ -26,8 +30,10 @@ const getAllFiles = async (dirPath, filter) => {
 			const filePath = joinPath(directory, file);
 			const info = await stat(filePath);
 
+			// Ignore symbolic links to avoid an endless loop
 			if (info.isSymbolicLink()) continue;
 
+			// Search subdirectories or determine if the file should get added to the result
 			if (info.isDirectory()) {
 				directories.push(filePath);
 				continue;
@@ -40,30 +46,38 @@ const getAllFiles = async (dirPath, filter) => {
 	return result;
 };
 
+// Get all js files from the manifest and convert file suffixes to ts
 const getManifestEntryPoints = () => {
 	const content = manifest.content_scripts?.flatMap((cs) => cs.js).filter(Boolean) ?? [];
 	const background = manifest.background?.scripts ?? [];
 
 	return [...content, ...background].map((path) => {
+		// Replace the last two characters with "ts" "script.js" -> "script.ts"
 		path = path.substring(0, path.length - 2) + 'ts';
+
+		// Convert the path to "src/extension/path"
 		if (path.startsWith('./')) return 'src' + path.substring(1);
 		if (path.startsWith('/')) return 'src' + path;
 		return 'src/' + path;
 	});
 };
 
+// Get all ts files from src/inject
 const getInjectEntryPoints = async () => {
 	return await getAllFiles('src/inject', (path) => path.endsWith('.ts'));
 };
 
+// Get all script.ts files from src/popup
 const getPopupEntryPoints = async () => {
 	return await getAllFiles('src/popup', (path) => path.endsWith('/script.ts'));
 };
 
+// Get all script.ts files from src/pages
 const getPageEntryPoints = async () => {
 	return await getAllFiles('src/pages', (path) => path.endsWith('/script.ts'));
 };
 
+// List of all entry points passed to esbuild
 const entryPoints = [
 	...getManifestEntryPoints(),
 	...await getInjectEntryPoints(),
@@ -71,8 +85,10 @@ const entryPoints = [
 	...await getPageEntryPoints()
 ];
 
+// Remove old build
 await remove('build', { recursive: true, force: true });
 
+// Build all entry points into build using esbuild
 build({
 	bundle: true,
 	entryNames: '[dir]/[name]',
@@ -84,6 +100,7 @@ build({
 	target: ['firefox113']
 });
 
+// Copy all non ts files to build (src/types and src/lib are also excluded)
 copy('src', 'build', {
 	recursive: true,
 	filter: (source, destination) => !source.endsWith('.ts') && !['src/types', 'src/lib'].includes(source)
