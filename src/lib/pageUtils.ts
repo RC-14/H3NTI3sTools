48f0e43d7d670bcf9473aa019f7d314ae2f07951
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-05-21 01:15:41 +0200

    Create pageUtils.ts to take advantage of base.css

---
+++ src/lib/pageUtils.ts
@@ -0,0 +1,35 @@
+/**
+ * Adds the CSS hidden class to the elements class list.
+ * 
+ * @param element The element you want to hide.
+ */
+export const hideElement = (element: HTMLElement) => {
+	element.classList.add('hidden');
+};
+
+/**
+ * Removes the CSS hidden class from the elements class list.
+ * 
+ * @param element The element you want to show.
+ */
+export const showElement = (element: HTMLElement) => {
+	element.classList.remove('hidden');
+};
+
+/**
+ * Adds the CSS hidden class to the elements class list.
+ * 
+ * @param element The element you want to hide.
+ */
+export const makeElementInvisible = (element: HTMLElement) => {
+	element.classList.add('invisible');
+};
+
+/**
+ * Removes the CSS hidden class from the elements class list.
+ * 
+ * @param element The element you want to show.
+ */
+export const makeElementVisible = (element: HTMLElement) => {
+	element.classList.remove('invisible');
+};
