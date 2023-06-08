fba4000bf3296a79e51eff82e5c4ee5264cae461
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-06-08 13:56:01 +0200

    Register kemono download handler in handlerMap

--- src/background/viewer/downloader/handlers/index.ts
+++ src/background/viewer/downloader/handlers/index.ts
@@ -1,8 +1,10 @@
+import kemono from './kemono';
 import nhentai from './nhentai';
 import pixiv from './pixiv';
 import { DownloadHandler } from '/src/lib/viewer';
 
 const handlerMap = new Map<string, DownloadHandler>();
+handlerMap.set('kemono.party', kemono);
 handlerMap.set('nhentai.net', nhentai);
 handlerMap.set('pixiv.net', pixiv);
 handlerMap.set('piximg.net', pixiv);
