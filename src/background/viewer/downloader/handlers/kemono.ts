b6cd9e58cad277110eac658171903993144e102c
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-06-08 13:55:32 +0200

    Create kemono download handler

---
+++ src/background/viewer/downloader/handlers/kemono.ts
@@ -0,0 +1,60 @@
+import { z } from 'zod';
+import { decode } from '/src/lib/htmlCharReferences';
+import { DownloadHandler } from '/src/lib/viewer';
+
+const creatorChache = new Map<string, string>();
+
+const apiResponseSchema = z.array(z.object({
+	title: z.string(),
+	content: z.string(),
+	attachments: z.array(z.object({
+		path: z.string()
+	})),
+}));
+
+const handler: DownloadHandler = {
+	media: async (url) => {
+		const apiUrl = new URL(url);
+		apiUrl.pathname = '/api' + apiUrl.pathname;
+
+		const apiResponse = await fetch(apiUrl).then((response) => response.json());
+		const parsedApiResponse = apiResponseSchema.parse(apiResponse).at(0);
+
+		if (parsedApiResponse === undefined) throw new Error(`Empty api response for: ${apiUrl.href}`);
+
+		const attachments = parsedApiResponse.attachments.filter((item) => ['png', 'jpg', 'gif', 'webp'].includes(item.path.split('.').at(-1)!));
+
+		if (attachments.length === 0) throw new Error("No attachments.");
+
+		const creatorUrl = new URL(url);
+		creatorUrl.pathname = creatorUrl.pathname.split('/post/')[0];
+
+		if (!creatorChache.has(creatorUrl.pathname)) {
+			const html = await fetch(creatorUrl, {
+				redirect: 'error'
+			}).then((response) => response.text());
+			
+			const creator = html.split('<span itemprop="name">')[1].split('</span>')[0];
+			creatorChache.set(creatorUrl.pathname, creator);
+		}
+
+		return {
+			origin: url,
+			name: parsedApiResponse.title,
+			description: decode(parsedApiResponse.content.replaceAll(/<[^>]+>/g, '')),
+			type: 'gallery',
+			sources: attachments.map((item) => 'https://c1.kemono.party/data' + item.path) as [string, ...string[]],
+			favorite: false,
+			tags: [],
+			creatorNames: [creatorChache.get(creatorUrl.pathname)!]
+		};
+	},
+	data: (url) => new Promise((resolve, reject) => setTimeout(async () => {
+		resolve({
+			source: url,
+			blob: await fetch(url).then((response) => response.blob())
+		});
+	}, 1_000))
+};
+
+export default handler;
