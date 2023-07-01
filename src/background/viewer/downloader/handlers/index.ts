9b8a14b88b02ec95fdb58ab95de9169528f28cc9
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-07-01 16:48:29 +0200

    Add flamescans handler to handler map

--- src/background/viewer/downloader/handlers/index.ts
+++ src/background/viewer/downloader/handlers/index.ts
@@ -1,4 +1,5 @@
 import asurascans from './asurascans';
+import flamescans from './flamescans';
 import kemono from './kemono';
 import nhentai from './nhentai';
 import pixiv from './pixiv';
@@ -6,6 +7,7 @@ import { DownloadHandler } from '/src/lib/viewer';
 
 const handlerMap = new Map<string, DownloadHandler>();
 handlerMap.set('asurascans.com', asurascans);
+handlerMap.set('flamescans.org', flamescans);
 handlerMap.set('asura.gg', asurascans);
 handlerMap.set('kemono.party', kemono);
 handlerMap.set('nhentai.net', nhentai);
