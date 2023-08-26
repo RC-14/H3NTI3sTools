5f9ce9d0646cca4da8a357641b0726596a8760d3
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-08-27 01:51:45 +0200

    Add shortcuts and img styling for hentai2read

---
+++ src/content/hentai2read/main.ts
@@ -0,0 +1,7 @@
+import { addNextPreviousShortcuts } from '/src/lib/nextPreviousShortcuts';
+import { qs } from '/src/lib/utils';
+
+const next = () => qs<HTMLButtonElement>('button.js-page_next')?.click();
+const previous = () => qs<HTMLButtonElement>('button.js-page_previous')?.click();
+
+addNextPreviousShortcuts(next, previous);
