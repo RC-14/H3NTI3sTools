11c942a5a1423c4c84e2f125b3809efa6e65b802
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-08-27 00:00:03 +0200

    Enable dark mode on kenmei

---
+++ src/content/kenmei/main.ts
@@ -0,0 +1,12 @@
+const DARK_MODE_CLASS = 'dark'
+
+const enableDarkMode = () => {
+	if (!document.documentElement.classList.contains(DARK_MODE_CLASS)) document.documentElement.classList.add(DARK_MODE_CLASS);
+};
+enableDarkMode();
+
+// Make sure it doesn't get turned off
+const observer = new MutationObserver(enableDarkMode);
+observer.observe(document.documentElement, {
+	attributeFilter: ['class']
+});
