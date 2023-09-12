af7db12e60487a76821db379bdf175124c631dba
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-09-13 00:26:51 +0200

    Make historyRecorder actually work

--- src/background/historyRecorder/index.ts
+++ src/background/historyRecorder/index.ts
@@ -1,13 +1,15 @@
 import { History, history, runtime, tabs } from 'webextension-polyfill';
 import { BackgroundFragment } from '/src/lib/fragments';
 
-const EXTENSION_BASE_URL = `moz-extension://${runtime.id}/`;
-const HISTORY_ENTRY_PROTOCOL = 'h3nti3:';
+const EXTENSION_BASE_URL = runtime.getURL('');
+const HISTORY_ENTRY_PROTOCOL = 'ext+h3nti3:';
 
 tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-	if (!changeInfo.url?.startsWith(HISTORY_ENTRY_PROTOCOL)) return;
+	if (changeInfo.status !== 'complete' || !tab.url?.startsWith(`${EXTENSION_BASE_URL}${encodeURIComponent(HISTORY_ENTRY_PROTOCOL)}`)) return;
 
-	await tabs.update(tabId, { url: `${EXTENSION_BASE_URL}${changeInfo.url.substring(HISTORY_ENTRY_PROTOCOL.length)}` });
+	await tabs.update(tabId, {
+		url: EXTENSION_BASE_URL + atob(decodeURIComponent(tab.url.split(encodeURIComponent(HISTORY_ENTRY_PROTOCOL))[1]))
+	});
 });
 
 const fragment: BackgroundFragment = {
@@ -17,7 +19,7 @@ const fragment: BackgroundFragment = {
 		if (!sender.url.startsWith(EXTENSION_BASE_URL)) throw new Error(`[historyRecorder] Message didn't come from a page of this extension.`);
 
 		const historyEntry: History.AddUrlDetailsType = {
-			url: `${HISTORY_ENTRY_PROTOCOL}${sender.url.substring(EXTENSION_BASE_URL.length)}`
+			url: `${HISTORY_ENTRY_PROTOCOL}${btoa(sender.url.substring(EXTENSION_BASE_URL.length))}`
 		};
 		if (sender.tab?.title !== undefined) historyEntry.title = sender.tab.title;
 
