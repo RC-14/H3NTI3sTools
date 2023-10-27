324644e9281ecfa02997b3dca675fe728c76426e
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-10-28 00:18:35 +0200

    Make kenmei remember me work for real this time

--- src/content/kenmei/main.ts
+++ src/content/kenmei/main.ts
@@ -8,11 +8,26 @@ const enableDarkMode = () => {
 enableDarkMode();
 
 // Make sure it doesn't get turned off
-const observer = new MutationObserver(enableDarkMode);
-observer.observe(document.documentElement, {
-	attributeFilter: ['class']
+const documentObserver = new MutationObserver(enableDarkMode);
+documentObserver.observe(document.documentElement, {
+	attributeFilter: ['class'],
 });
 
 // Automatically check the remember me checkbox in case I need to log in again.
 const rememberMeCheckbox = qs<HTMLInputElement>('input[type="checkbox"][data-v-55ca7f47]');
-if (rememberMeCheckbox?.checked === false) rememberMeCheckbox.click(); // The proper styling for the checkbox only gets applied on click
+if (rememberMeCheckbox?.checked === false) {
+	const dialog = qs('div[role="dialog"]');
+	if (!dialog) throw new Error(`Couldn't find a dialog to check remember me in.`);
+
+	const dialogObserver = new MutationObserver((mutations, observer) => {
+		// The proper styling for the checkbox only gets applied on click
+		rememberMeCheckbox.click();
+
+		// Only needed once
+		observer.disconnect();
+	});
+
+	dialogObserver.observe(dialog, {
+		attributeFilter: ['style']
+	});
+}
