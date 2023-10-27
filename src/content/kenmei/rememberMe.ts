a800352e18a3d443fb449ab77fd972ced4e49170
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-10-28 00:43:34 +0200

    Now really working (fully tested) auto remember me

--- src/content/kenmei/rememberMe.ts
+++ src/content/kenmei/rememberMe.ts
@@ -1,14 +1,18 @@
-import { qs } from '/src/lib/utils';
+import { qs, runAfterReadyStateReached } from '/src/lib/utils';
 
-// Automatically check the remember me checkbox in case I need to log in again.
-const rememberMeCheckbox = qs<HTMLInputElement>('input[type="checkbox"][data-v-55ca7f47]');
-if (rememberMeCheckbox?.checked === false) {
+const getCheckbox = () => qs<HTMLInputElement>('input[type="checkbox"][data-v-55ca7f47]');
+
+runAfterReadyStateReached('complete', () => {
+	// Automatically check the remember me checkbox in case I need to log in again.
+	const rememberMeCheckbox = getCheckbox();
+	if (rememberMeCheckbox?.checked !== false) return;
+
 	const dialog = qs('div[role="dialog"]');
 	if (!dialog) throw new Error(`Couldn't find a dialog to check remember me in.`);
 
 	const observer = new MutationObserver((mutations, observer) => {
 		// The proper styling for the checkbox only gets applied on click
-		rememberMeCheckbox.click();
+		setTimeout(() => getCheckbox()?.click(), 10);
 
 		// Only needed once
 		observer.disconnect();
@@ -17,4 +21,4 @@ if (rememberMeCheckbox?.checked === false) {
 	observer.observe(dialog, {
 		attributeFilter: ['style']
 	});
-}
+});
