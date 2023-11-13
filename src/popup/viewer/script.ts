c1a72df70ea83de12740e832e472e8c7bd29e7c9
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-11-14 00:38:35 +0100

    Add a button to open current tab in presentation

--- src/popup/viewer/script.ts
+++ src/popup/viewer/script.ts
@@ -1,8 +1,27 @@
+import { runtime, tabs, type Tabs } from 'webextension-polyfill';
 import '/src/lib/devToolHelpers';
 import { qs, sendRuntimeMessage } from '/src/lib/utils';
+import type { ShowMediaMessage } from '/src/lib/viewer';
 
 const cleanupButton = qs<HTMLButtonElement>('button#cleanup-button');
-if (!(cleanupButton instanceof HTMLButtonElement)) throw new Error(`Didn't find cleanup button.`);
+const openTabInPresentationButton = qs<HTMLButtonElement>('button#open-tab-in-presentation');
+
+let tab: Tabs.Tab | undefined = undefined;
+
+if (!(
+	cleanupButton &&
+	openTabInPresentationButton
+)) throw new Error(`Not all elements were found before adding functionality.`);
+
+// Enable open tab in presentation button
+tabs.query({ active: true, currentWindow: true }).then((tabArray) => {
+	tab = tabArray[0];
+
+	if (!tab) return;
+	if (!tab.url || tab.url.startsWith(runtime.getURL(''))) return;
+
+	openTabInPresentationButton.disabled = false;
+});
 
 cleanupButton.addEventListener('click', async (event) => {
 	cleanupButton.disabled = true;
@@ -11,4 +30,19 @@ cleanupButton.addEventListener('click', async (event) => {
 	await sendRuntimeMessage('background', 'viewer', 'cleanup');
 
 	cleanupButton.innerText = 'Done!';
+});
+
+openTabInPresentationButton.addEventListener('click', async (event) => {
+	openTabInPresentationButton.disabled = true;
+
+	if (!tab?.url) throw new Error("Couldn't get the URL of the active Tab.");
+
+	const data: ShowMediaMessage = {
+		origins: [tab.url],
+		targetTab: tab.pinned ? -1 : -2
+	};
+
+	await sendRuntimeMessage('background', 'viewer', 'showMedia', data);
+
+	window.parent.close();
 });
