a4a9160fda8fe9a53f62281b978a7abf0e028da2
RC-14 <61058098+RC-14@users.noreply.github.com>
2024-02-14 23:31:41 +0100

    Fix mangahub img download

--- src/background/viewer/downloader/handlers/mangahub.ts
+++ src/background/viewer/downloader/handlers/mangahub.ts
@@ -4,7 +4,7 @@ import { dataHandler } from './genericDataHandler';
 import { COLLECTION_OS_NAME, CollectionSchema, createCollection, getFromObjectStore, getViewerIDB, type DownloadHandler } from '/src/lib/viewer';
 
 const API_URL = 'https://mangahub.io/api/graphql';
-const IMG_URL_BASE = 'https://imgx.mangahub.io/';
+const IMG_URL_BASE = 'https://imgx.mghcdn.com/';
 const ACCESS_TOKEN_URL = 'https://mangahub.io/';
 const ACCESS_TOKEN_COOKIE_NAME = 'mhub_access';
 let lastError = 0;
