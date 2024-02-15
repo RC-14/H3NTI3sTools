7dcb4705061f5f8d4151c047cc8b810c7191dfc7
RC-14 <61058098+RC-14@users.noreply.github.com>
2024-02-15 23:41:43 +0100

    Abide by Kemonos Rate Limit

--- src/background/viewer/downloader/handlers/kemono.ts
+++ src/background/viewer/downloader/handlers/kemono.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import { dataHandler } from './genericDataHandler';
 import { decode } from '/src/lib/htmlCharReferences';
-import type { DownloadHandler } from '/src/lib/viewer';
+import type { Data, DownloadHandler, Media } from '/src/lib/viewer';
 
 const ALLOWED_FILE_TYPES = ['png', 'jpg', 'gif', 'webp'];
 
@@ -18,53 +18,60 @@ const apiResponseSchema = z.object({
 	})
 });
 
-const isAllowedFileType = (path: string) => ALLOWED_FILE_TYPES.includes(path.split('.').at(-1)!)
+const isAllowedFileType = (path: string) => ALLOWED_FILE_TYPES.includes(path.split('.').at(-1)!);
+
+const mediaHandler = async (url: string): Promise<Media> => {
+	const apiUrl = new URL(url);
+	apiUrl.pathname = '/api/v1' + apiUrl.pathname;
+
+	const apiResponse = await fetch(apiUrl).then((response) => response.json());
+	const parsedApiResponse = apiResponseSchema.parse(apiResponse);
+
+	const attachmentPaths = parsedApiResponse.attachments.filter((item) => isAllowedFileType(item.path)).map(item => item.path);
+	if (
+		parsedApiResponse.file.path !== undefined &&
+		isAllowedFileType(parsedApiResponse.file.path) &&
+		!attachmentPaths.includes(parsedApiResponse.file.path)
+	) {
+		attachmentPaths.unshift(parsedApiResponse.file.path);
+	}
+
+	if (attachmentPaths.length === 0) throw new Error(`No attachments for post: ${url}`);
+
+	const creatorUrl = new URL(url);
+	creatorUrl.pathname = creatorUrl.pathname.split('/post/')[0]!;
+
+	if (!creatorChache.has(creatorUrl.pathname)) {
+		const html = await fetch(creatorUrl, {
+			redirect: 'error'
+		}).then((response) => response.text());
+
+		const creator = html.split('<span itemprop="name">')[1]?.split('</span>')[0]?.trim();
+		if (creator === undefined || creator.length === 0) throw new Error(`Couldn't parse creator from kemono html.`);
+
+		creatorChache.set(creatorUrl.pathname, creator);
+	}
+
+	return {
+		origin: url,
+		name: parsedApiResponse.title,
+		description: decode(parsedApiResponse.content.replaceAll(/<[^>]+>/g, '')) || undefined,
+		type: 'gallery',
+		sources: attachmentPaths.map((path) => 'https://c1.kemono.party/data' + path),
+		favorite: false,
+		tags: [],
+		creatorNames: [creatorChache.get(creatorUrl.pathname)!]
+	};
+};
+
+const generateRateLimitedHandler = <T extends Media | Data>(handler: (url: string) => Promise<T>, maxReqPerSec: number) => {
+	const delay = 1e3 / maxReqPerSec;
+	return (url: string) => new Promise<T>((res, rej) => setTimeout(() => handler(url).then(res).catch(rej), delay));
+};
 
 const handler: DownloadHandler = {
-	media: async (url) => {
-		const apiUrl = new URL(url);
-		apiUrl.pathname = '/api/v1' + apiUrl.pathname;
-
-		const apiResponse = await fetch(apiUrl).then((response) => response.json());
-		const parsedApiResponse = apiResponseSchema.parse(apiResponse);
-
-		const attachmentPaths = parsedApiResponse.attachments.filter((item) => isAllowedFileType(item.path)).map(item => item.path);
-		if (
-			parsedApiResponse.file.path !== undefined &&
-			isAllowedFileType(parsedApiResponse.file.path) &&
-			!attachmentPaths.includes(parsedApiResponse.file.path)
-		) {
-			attachmentPaths.unshift(parsedApiResponse.file.path);
-		}
-
-		if (attachmentPaths.length === 0) throw new Error(`No attachments for post: ${url}`);
-
-		const creatorUrl = new URL(url);
-		creatorUrl.pathname = creatorUrl.pathname.split('/post/')[0]!;
-
-		if (!creatorChache.has(creatorUrl.pathname)) {
-			const html = await fetch(creatorUrl, {
-				redirect: 'error'
-			}).then((response) => response.text());
-
-			const creator = html.split('<span itemprop="name">')[1]?.split('</span>')[0]?.trim();
-			if (creator === undefined || creator.length === 0) throw new Error(`Couldn't parse creator from kemono html.`);
-
-			creatorChache.set(creatorUrl.pathname, creator);
-		}
-
-		return {
-			origin: url,
-			name: parsedApiResponse.title,
-			description: decode(parsedApiResponse.content.replaceAll(/<[^>]+>/g, '')) || undefined,
-			type: 'gallery',
-			sources: attachmentPaths.map((path) => 'https://c1.kemono.party/data' + path),
-			favorite: false,
-			tags: [],
-			creatorNames: [creatorChache.get(creatorUrl.pathname)!]
-		};
-	},
-	data: (url) => new Promise((resolve, reject) => setTimeout(() => dataHandler(url).then(resolve).catch(reject), 1_000))
+	media: generateRateLimitedHandler(mediaHandler, 1),
+	data: generateRateLimitedHandler(dataHandler, 1)
 };
 
 export default handler;
